e6a3de0a78f9969e75a4c3607aa672d5982952e6
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-04-05 21:05:11 -0300

    :heavy_check_mark: feat Requisite two e three finished.
    Co-authored-by: Joarez Ximenes <JoarezXimenes@users.noreply.github.com>
    Co-authored-by: Leonardo <leodiman182@users.noreply.github.com>
    Co-authored-by: Leonardo Begnossi <MadOrchid@users.noreply.github.com>

--- src/services/fecthApi.js
+++ src/services/fecthApi.js
@@ -1,19 +1,21 @@
-const fetchApi = async (questionNumber) => {
+export const fetchToken = async () => {
   const ENDPOINT_TOKEN = 'https://opentdb.com/api_token.php?command=request';
-  const responseToken = await fetch(ENDPOINT_TOKEN);
-  const jsonToken = await responseToken.json();
-  const token = (responseToken.ok
-    ? Promise.resolve(jsonToken)
-    : Promise.reject(jsonToken));
-  token.then((e) => console.log(e));
+  const response = await fetch(ENDPOINT_TOKEN);
+  const json = await response.json();
+  return (response.ok
+    ? Promise.resolve(json.token)
+    : Promise.reject(json));
+};
 
-  const ENDPOINT = `https://opentdb.com/api.php?amount=${questionNumber}&token=${token.then(({ tokenU }) => tokenU)}`;
+export const fetchApi = async (numberOfQuestions, tokenKey) => {
+  const { token } = tokenKey;
+  console.log(token);
+  const ENDPOINT = `https://opentdb.com/api.php?amount=${numberOfQuestions}&token=${token}`;
+  console.log(ENDPOINT);
   const response = await fetch(ENDPOINT);
   const json = await response.json();
-  json.then((el) => console.log(el));
+  // json.then((el) => console.log(el));
   return (response.ok
     ? Promise.resolve(json)
     : Promise.reject(json));
 };
-
-export default fetchApi;
